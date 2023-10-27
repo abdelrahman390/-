e2d2d30a138495a7d38ae2a4b96c65b1d695573b
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2023-10-27 20:21:24 +0200

    Add files via upload

---
+++ assets/javascript/account/account-new-password.js
@@ -0,0 +1,48 @@
+let inputs = document.querySelectorAll('.max-width input'),
+    alarmMessage = document.querySelector('.max-width .alarm'),
+    button = document.querySelector('.max-width .container .lift button');
+// accountNewPasswordButtons = document.querySelector(' .max-width .container .lift button');
+
+// console.log(inputs)
+// console.log(alarmMessage)
+// console.log(button)
+// console.log(accountNewPasswordButtons)
+
+inputs.forEach(element => {
+    element.addEventListener('input', function (event) {
+        function handleClick() {
+
+
+            let inputsCheckState = inputs[0].value.length !== 0 && inputs[1].value.length !== 0 && inputs[2].value.length !== 0;
+
+            if (inputsCheckState === true) {
+
+                if (inputs[1].value.length === inputs[2].value.length) {
+
+                    if (inputs[1].value === inputs[2].value) {
+                        button.style.cssText = 'opacity: 1; pointer-events: painted;';
+                        alarmMessage.style.cssText += 'display: none; padding: 0; background-color: unset;';
+                        inputs[2].style.cssText = 'border: 1px solid var(--primary-color); margin-bottom: 0;';
+                    } else {
+                        button.style.cssText = 'opacity: 0.5; pointer-events: none;';
+                        alarmMessage.style.cssText += 'display: unset;background-color: unset;';
+                        inputs[2].style.cssText = 'border: 2px solid red; margin-bottom: 30px;';
+                    }
+                } else if (inputs[1].value.length < inputs[2].value.length) {
+                    button.style.cssText = 'opacity: 0.5; pointer-events: none;';
+                    alarmMessage.style.cssText += 'display: unset;background-color: unset;';
+                    inputs[2].style.cssText = 'border: 2px solid red; margin-bottom: 30px;';
+                } else {
+                    button.style.cssText = 'opacity: 0.5; pointer-events: none;'
+                }
+            } else {
+                button.style.cssText = 'opacity: 0.5; pointer-events: none;'
+            }
+        }
+        handleClick()
+    });
+});
+
+button.onclick = () => {
+    doneMessage('../../../assets/images/Done.webp', 'تم تغيير كلمة المرور بنجاح', "../../../pages/account/account.html", 'حسناً')
+}
