8e2cc76e480f47399806281b91be94b89e8ac2dc
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2023-10-28 12:57:28 +0200

    Delete assets/javascript directory

--- assets/javascript/account/admin/account-admin.js
+++
@@ -1,5 +0,0 @@
-const accountButtons = document.querySelector('.account .max-width .container .lift button');
-
-accountButtons.onclick = () => {
-    doneMessage('../../../assets/images/Done.webp', 'تمت الحفظ بنجاح', "", 'حسناً')
-}
