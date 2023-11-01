55a6a4a4b26022e839fe898da0c676c78f35fe7e
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2023-11-01 11:09:51 +0200

    Add files via upload

---
+++ assets/javascript/mainPage.js
@@ -0,0 +1,7 @@
+let mainBAckGroundImg = document.querySelectorAll(".mainPage .mainPageImgs .container .img-cont img")
+
+if (width < 601) {
+    mainBAckGroundImg.forEach(element => {
+        element.src = "assets/images/mainPage-phone.webp"
+    });
+}
